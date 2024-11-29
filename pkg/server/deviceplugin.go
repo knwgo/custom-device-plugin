68d127179a15a4b6769df6f6510c8181a4bfa733
knwgo <kaynw@qq.com>
2024-11-29 16:04:41 +0800

    support PreferredAllocation

--- pkg/server/deviceplugin.go
+++ pkg/server/deviceplugin.go
@@ -2,20 +2,24 @@ package server
 
 import (
 	"context"
+	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"k8s.io/klog/v2"
 	pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
+
+	"github.com/knwgo/custom-device-plugin/pkg/utils"
 )
 
 func (d *DPServer) GetDevicePluginOptions(_ context.Context, _ *pluginapi.Empty) (*pluginapi.DevicePluginOptions, error) {
 	klog.Info("GetDevicePluginOptions called")
 	return &pluginapi.DevicePluginOptions{
 		PreStartRequired:                true,
-		GetPreferredAllocationAvailable: false,
+		GetPreferredAllocationAvailable: true,
 	}, nil
 }
 
@@ -57,7 +61,75 @@ func (d *DPServer) ListAndWatch(_ *pluginapi.Empty, server pluginapi.DevicePlugi
 
 func (d *DPServer) GetPreferredAllocation(_ context.Context, request *pluginapi.PreferredAllocationRequest) (*pluginapi.PreferredAllocationResponse, error) {
 	klog.Info("GetPreferredAllocation called")
-	return nil, nil
+
+	resp := &pluginapi.PreferredAllocationResponse{ContainerResponses: make([]*pluginapi.ContainerPreferredAllocationResponse, len(request.ContainerRequests))}
+
+	getPreferred := func(allocationRequest *pluginapi.ContainerPreferredAllocationRequest) (*pluginapi.ContainerPreferredAllocationResponse, error) {
+		r := &pluginapi.ContainerPreferredAllocationResponse{DeviceIDs: make([]string, 0)}
+		r.DeviceIDs = append(r.DeviceIDs, allocationRequest.MustIncludeDeviceIDs...)
+		mustMap := make(map[string]struct{})
+		for i := range allocationRequest.MustIncludeDeviceIDs {
+			mustMap[allocationRequest.MustIncludeDeviceIDs[i]] = struct{}{}
+		}
+
+		d.m.Lock()
+		defer d.m.Unlock()
+
+		sd := make(utils.SortableDeviceList, len(allocationRequest.AvailableDeviceIDs))
+		for i, id := range allocationRequest.AvailableDeviceIDs {
+			dev, exist := d.devices[id]
+			if !exist {
+				return nil, status.Errorf(codes.InvalidArgument, "device %s not found", id)
+			}
+
+			f, err := os.Open(filepath.Join(d.devicePath, dev.Meta.Filename))
+			if err != nil {
+				return nil, status.Errorf(codes.Internal, "failed to open file: %s", filepath.Join(d.devicePath, dev.Meta.Filename))
+			}
+
+			fi, err := f.Stat()
+			if err != nil {
+				return nil, status.Errorf(codes.Internal, "failed to get file %s stat", filepath.Join(d.devicePath, dev.Meta.Filename))
+			}
+
+			sd[i] = utils.SortableDevice{
+				ID:      id,
+				ModTime: fi.ModTime(),
+			}
+		}
+
+		sort.Sort(sd)
+		amount := int(allocationRequest.AllocationSize) - len(allocationRequest.MustIncludeDeviceIDs)
+		for _, dev := range sd {
+			if amount <= 0 {
+				break
+			}
+
+			if _, ok := mustMap[dev.ID]; ok {
+				continue
+			}
+
+			r.DeviceIDs = append(r.DeviceIDs, dev.ID)
+			amount--
+		}
+
+		if amount > 0 {
+			return nil, status.Errorf(codes.InvalidArgument, "len AvailableDeviceIDs: %d, len MustIncludeDeviceIDs: %d, AllocationSize: %d",
+				len(allocationRequest.AvailableDeviceIDs), len(allocationRequest.MustIncludeDeviceIDs), allocationRequest.AllocationSize)
+		}
+
+		return r, nil
+	}
+
+	for i, req := range request.ContainerRequests {
+		res, err := getPreferred(req)
+		if err != nil {
+			return nil, err
+		}
+		resp.ContainerResponses[i] = res
+	}
+
+	return resp, nil
 }
 
 func (d *DPServer) Allocate(_ context.Context, request *pluginapi.AllocateRequest) (*pluginapi.AllocateResponse, error) {
--- pkg/utils/types.go
+++ pkg/utils/types.go
@@ -1,6 +1,10 @@
 package utils
 
-import pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
+import (
+	"time"
+
+	pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
+)
 
 type DeviceData struct {
 	NumaNodes []int64 `json:"Nodes"`
@@ -14,4 +18,17 @@ type DeviceMeta struct {
 type Device struct {
 	Meta   DeviceMeta
 	Entity *pluginapi.Device
+}
+
+type SortableDevice struct {
+	ID      string
+	ModTime time.Time
+}
+
+type SortableDeviceList []SortableDevice
+
+func (s SortableDeviceList) Len() int      { return len(s) }
+func (s SortableDeviceList) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s SortableDeviceList) Less(i, j int) bool {
+	return s[i].ModTime.Before(s[j].ModTime)
 }
